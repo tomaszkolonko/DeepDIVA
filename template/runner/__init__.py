47bbddf8ea34acddf19d1dceb5ffc1f77519c8fe
Tomasz Kolonko <tz.kolonko@gmail.com>
2018-11-02 00:11:36 +0100

    adding a new runner for segmentation dataset

--- template/runner/__init__.py
+++ template/runner/__init__.py
@@ -2,5 +2,6 @@ from .apply_model import ApplyModel
 from .bidimensional import Bidimensional
 from .image_classification import ImageClassification
 from .triplet import Triplet
+from .image_segmentation import ImageSegmentation
 
-__all__ = ['ImageClassification', 'Bidimensional', 'Triplet', 'ApplyModel']
+__all__ = ['ImageClassification', 'ImageSegmentation', 'Bidimensional', 'Triplet', 'ApplyModel']
