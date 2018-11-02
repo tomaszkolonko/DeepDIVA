fcd3544ad00df2fd8543c5292f5510e4073e2f05
LindaSt <lin.studer@gmail.com>
2018-11-02 18:40:10 +0100

    working on data split for train set

--- testing.py
+++ testing.py
@@ -53,3 +53,70 @@ def one_hot_to_output(matrix, out_path):
     # save image
     img = Image.fromarray(RGB.astype('uint8'), 'RGB')
     img.save(out_path)
+
+
+def unzip_folders(zip_file, file, folder_extracted, name):
+    dirname = os.path.join(folder_extracted, name)
+    # unzip files
+    zip_file.extract(file, dirname)
+    with zipfile.ZipFile(os.path.join(dirname, file), "r") as zip_ref:
+        zip_ref.extractall(dirname)
+        # delete zips
+        os.remove(os.path.join(dirname, file))
+
+
+def download_HisDB(link, out_folder_name):
+    download_path = os.path.join(path_to_save, link.geturl().rsplit('/', 1)[-1])
+    # download files
+    # urllib.request.urlretrieve(link.geturl(), download_path)
+    folder_extracted = os.path.splitext(os.path.join(path_to_save, link.geturl().rsplit('/', 1)[-1].rsplit('.', 1)[0]))[
+        0]
+
+    # unpack relevant folders
+    zip_file = zipfile.ZipFile(download_path)
+
+    # unpack imgs and gt
+    for file in zip_file.namelist():
+        if file.startswith('img'):
+            unzip_folders(zip_file, file, folder_extracted, 'imgs')
+        elif file.startswith('pixel'):
+            unzip_folders(zip_file, file, folder_extracted, 'gt')
+
+    for subdir, dirs, files in os.walk(folder_extracted):
+        for file in files:
+            shutil.move(os.path.join(subdir, file), os.path.join(os.path.dirname(subdir), file))
+
+    if out_folder_name == 'train':
+        train_folder = os.path.join(os.path.dirname(folder_extracted), out_folder_name)
+
+    os.rename(folder_extracted, os.path.join(os.path.dirname(folder_extracted), out_folder_name))
+
+
+def hisDB(path_to_save):
+    """
+    Fetches and prepares (in a DeepDIVA friendly format) the HisDB dataset for semantic segmentation to the location specified
+    on the file system
+
+    Parameters
+    ----------
+    args : dict
+        List of arguments necessary to run this routine. In particular its necessary to provide
+        output_folder as String containing the path where the dataset will be downloaded
+
+    Returns
+    -------
+        None
+    """
+    # links to HisDB
+    linktrain_all = urllib.parse.urlparse(
+        'https://diuf.unifr.ch/main/hisdoc/sites/diuf.unifr.ch.main.hisdoc/files/uploads/diva-hisdb/hisdoc/all.zip')
+    linktest_all = urllib.parse.urlparse(
+        'https://diuf.unifr.ch/main/hisdoc/sites/diuf.unifr.ch.main.hisdoc/files/uploads/diva-hisdb/hisdoc/private-test/all-privateTest.zip')
+
+    download_HisDB(linktest_all, 'test')
+    download_HisDB(linktrain_all, 'train')
+
+
+path_to_save = '/home/linda/Documents/PhD/datasets/'
+hisDB(path_to_save)
+
