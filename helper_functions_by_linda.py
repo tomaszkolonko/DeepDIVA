1917ec6de658c53f9fc0ad33959d2ab5abcba705
LindaSt <lin.studer@gmail.com>
2018-11-07 20:06:31 +0100

    added one-hot (de)coding for single class for HisDB use only (multi-class functions still there, those are more generic)

--- helper_functions_by_linda.py
+++ helper_functions_by_linda.py
@@ -1,39 +1,42 @@
 from PIL import Image
 import numpy as np
 
+from sklearn.preprocessing import OneHotEncoder
+
 
 def int_to_one_hot(x, n_classes):
     """
     Read out class encoding from blue channel bit-encoding (1 to [0,0,0,1] -> length determined by the number of classes)
     :param x: integer (pixel value of Blue channel from RGB image)
     :param n_classes: number of class labels
-    :return: one-hot encoded list for
+    :return: (multi) one-hot encoded list for integer
     """
     s = '{0:0' + str(n_classes) + 'b}'
     return list(map(int, list(s.format(x))))
 
 
-def label_img_to_one_hot(np_array):
+def multi_label_img_to_one_hot(np_array):
     """
     TODO: There must be a faster way of doing this
-    Convert ground truth label image to one-hot encoded matrix of size image height x image width x #classes
+    Convert ground truth label image to multi-one-hot encoded matrix of size image height x image width x #classes
     :param np_array: numpy array of an RGB image
-    :return: sparse one-hot encoded class matrix
+    :return: sparse one-hot encoded multi-class matrix
     """
     im_np = np_array[:, :, 2].astype(np.int8)
     nb_classes = len(int_to_one_hot(im_np.max(), ''))
 
     class_dict = {x: int_to_one_hot(x, nb_classes) for x in np.unique(im_np)}
     # create the one hot matrix
-    one_hot_matrix = np.asanyarray([[class_dict[im_np[i,j]] for j in range(im_np.shape[1])] for i in range(im_np.shape[0])])
+    one_hot_matrix = np.asanyarray(
+        [[class_dict[im_np[i, j]] for j in range(im_np.shape[1])] for i in range(im_np.shape[0])])
 
     return one_hot_matrix
 
 
-def one_hot_to_output(matrix, out_path):
+def multi_one_hot_to_output(matrix, out_path):
     """
-    This function converts the one-hot encoded matrix to an image like it was provided in the ground truth
-    :param matrix: one-hot encoded matrix
+    This function converts the multi-one-hot encoded matrix to an image like it was provided in the ground truth
+    :param matrix: multi-one-hot encoded matrix
     :param out_path: path to output (e.g. image output/image1.png)
     """
     # create RGB
@@ -47,3 +50,49 @@ def one_hot_to_output(matrix, out_path):
     # save image
     img = Image.fromarray(RGB.astype('uint8'), 'RGB')
     img.save(out_path)
+
+
+def label_img_to_one_hot(np_array):
+    """
+    Convert ground truth label image to one-hot encoded matrix of size image height x image width x #classes
+    :param np_array: numpy array of an RGB image
+    :return: sparse one-hot encoded class matrix
+    """
+    im_np = np_array[:, :, 2].astype(np.int8)
+    
+    integer_encoded = np.array([i for i in range(8)])
+    onehot_encoder = OneHotEncoder(sparse=False)
+    integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+    onehot_encoded = onehot_encoder.fit_transform(integer_encoded).astype(np.int8)
+
+    replace_dict = {k:v for k,v in zip([1,2,4,6,8,10,12,14], onehot_encoded)}        
+    # create the one hot matrix
+    one_hot_matrix = np.asanyarray(
+        [[replace_dict[im_np[i, j]] for j in range(im_np.shape[1])] for i in range(im_np.shape[0])])
+        
+    return one_hot_matrix
+
+
+def one_hot_to_output(matrix, out_path):
+    """
+    This function converts the one-hot encoded matrix to an image like it was provided in the ground truth
+    :param matrix: one-hot encoded matrix
+    :param out_path: path to output (e.g. image output/image1.png)
+    """
+    matrix = np.char.mod('%d', matrix)
+    
+    integer_encoded = np.array([i for i in range(8)])
+    onehot_encoder = OneHotEncoder(sparse=False)
+    integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+    onehot_encoded = onehot_encoder.fit_transform(integer_encoded).astype(np.int8)
+    
+    replace_dict = {k:v for k,v in zip([''.join(i) for i in onehot_encoded.astype(str)], [1,2,4,6,8,10,12,14])}    
+
+    B = np.array([[replace_dict[''.join(matrix[i][j])] for j in range(matrix.shape[1])] for i in range(matrix.shape[0])])
+    
+    RGB = np.dstack((np.zeros(shape=(matrix.shape[0], matrix.shape[1], 2), dtype=np.int8), B))
+
+    # save image
+    img = Image.fromarray(RGB.astype('uint8'), 'RGB')
+    img.save(out_path)
+
