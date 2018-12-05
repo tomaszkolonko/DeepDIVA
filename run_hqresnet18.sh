763087b40441b254707af12a7acd4fae9d3785dc
Thomas Kolonko <tz.kolonko@gmail.com>
2018-12-05 14:50:35 +0100

    fixed double argument passing

--- run_hqresnet18.sh
+++ run_hqresnet18.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # hq_resnet18 ---------------------------------------
-python ./template/RunMe.py --dataset-folder ../ASBESTOS/ --output-folder ../hq_resnet18/ --model-name hqresnet18 --epochs 30 --experiment-name classification --output-folder ./resnet18/ --ignoregit --lr 0.01 --optimizer-name Adam --decay-lr 5 --seed 42
+python ./template/RunMe.py --dataset-folder ../ASBESTOS/ --output-folder ../hq_resnet18/ --model-name hqresnet18 --epochs 30 --experiment-name classification --ignoregit --lr 0.01 --optimizer-name Adam --decay-lr 5 --seed 42
 
-python ./template/RunMe.py --dataset-folder ../ASBESTOS/ --output-folder ../hq_resnet18/ --model-name hqresnet18 --epochs 30 --experiment-name classification --output-folder ./resnet18/ --ignoregit --lr 0.01 --optimizer-name Adam --decay-lr 5 --seed 42 --pretrained
+python ./template/RunMe.py --dataset-folder ../ASBESTOS/ --output-folder ../hq_resnet18/ --model-name hqresnet18 --epochs 30 --experiment-name classification --ignoregit --lr 0.01 --optimizer-name Adam --decay-lr 5 --seed 42 --pretrained
