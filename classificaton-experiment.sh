bce7556c0e64c5d00de8dc21bd864a6d07749e78
pinargoktepe <pinargoktepe@gmail.com>
2018-12-06 17:13:14 +0100

    scripts for similarity experiments are updated. classification-experiment.sh updated for inception_v3 model image_classification/train/py is updated for inception_v3 model

--- classificaton-experiment.sh
+++ classificaton-experiment.sh
@@ -1,40 +1,44 @@
 #!/bin/bash
 
 # Resnet18 ---------------------------------------
-python ./template/RunMe.py --runner-class image_classification --dataset-folder ../datasets/ICDAR2017-CLAMM/StyleClassification/ 
---model-name resnet18 --epochs 50 --experiment-name classification --output-folder ../output/ --ignoregit --lr 0.01 --momentum 0.9 
+python ./template/RunMe.py --runner-class image_classification --dataset-folder ../datasets/ICDAR2017-CLAMM/StyleClassification/ \
+--model-name resnet18 --epochs 50 --experiment-name classification --output-folder ../output/ --ignoregit --lr 0.01 --momentum 0.9 \
 --decay-lr 20
 
-python ./template/RunMe.py --runner-class image_classification --dataset-folder ../datasets/ICDAR2017-CLAMM/StyleClassification/ 
---model-name resnet18 --epochs 50 --experiment-name classification --output-folder ../output/ --ignoregit --lr 0.01 --momentum 0.9 
---decay-lr 20 --pretrained
+python ./template/RunMe.py --runner-class image_classification --dataset-folder ../datasets/ICDAR2017-CLAMM/StyleClassification/ \
+ --model-name resnet18 --epochs 50 --experiment-name classification --output-folder ../output/ --ignoregit --lr 0.01 --momentum 0.9 \
+ --decay-lr 20 --pretrained
 
 # VGG19 ---------------------------------------
-python ./template/RunMe.py --runner-class image_classification --dataset-folder ../datasets/ICDAR2017-CLAMM/StyleClassification/ 
---model-name vgg19 --epochs 50 --experiment-name classification --output-folder ../output/ --ignoregit --lr 0.01 --momentum 0.9 
---decay-lr 20
+python ./template/RunMe.py --runner-class image_classification --dataset-folder ../datasets/ICDAR2017-CLAMM/StyleClassification/ \
+ --model-name vgg19 --epochs 50 --experiment-name classification --output-folder ../output/ --ignoregit --lr 0.01 --momentum 0.9 \
+ --decay-lr 20
 
-python ./template/RunMe.py --runner-class image_classification --dataset-folder ../datasets/ICDAR2017-CLAMM/StyleClassification/
---model-name vgg19 --epochs 50 --experiment-name classification --output-folder ../output/ --ignoregit --lr 0.01 --momentum 0.9
---decay-lr 20 --pretrained
+python ./template/RunMe.py --runner-class image_classification --dataset-folder ../datasets/ICDAR2017-CLAMM/StyleClassification/ \
+ --model-name vgg19 --epochs 50 --experiment-name classification --output-folder ../output/ --ignoregit --lr 0.01 --momentum 0.9 \
+ --decay-lr 20 --pretrained
 
 # VGG19_bn ---------------------------------------
-python ./template/RunMe.py --runner-class image_classification --dataset-folder ../datasets/ICDAR2017-CLAMM/StyleClassification/
---model-name vgg19_bn --epochs 50 --experiment-name classification --output-folder ../output/ --ignoregit --lr 0.01 --momentum 0.9
---decay-lr 20
+python ./template/RunMe.py --runner-class image_classification --dataset-folder ../datasets/ICDAR2017-CLAMM/StyleClassification/ \
+ --model-name vgg19_bn --epochs 50 --experiment-name classification --output-folder ../output/ --ignoregit --lr 0.01 --momentum 0.9 \
+ --decay-lr 20
 
-python ./template/RunMe.py --runner-class image_classification --dataset-folder ../datasets/ICDAR2017-CLAMM/StyleClassification/
---model-name vgg19_bn --epochs 50 --experiment-name classification --output-folder ../output/ --ignoregit --lr 0.01 --momentum 0.9
---decay-lr 20 --pretrained
+python ./template/RunMe.py --runner-class image_classification --dataset-folder ../datasets/ICDAR2017-CLAMM/StyleClassification/ \
+ --model-name vgg19_bn --epochs 50 --experiment-name classification --output-folder ../output/ --ignoregit --lr 0.01 --momentum 0.9 \
+ --decay-lr 20 --pretrained
 
 # Densenet121 ---------------------------------------
-python ./template/RunMe.py --runner-class image_classification --dataset-folder ../datasets/ICDAR2017-CLAMM/StyleClassification/
---model-name densenet121 --epochs 50 --experiment-name classification --output-folder ../output/ --ignoregit --lr 0.01 --momentum 
-0.9
---decay-lr 20
+python ./template/RunMe.py --runner-class image_classification --dataset-folder ../datasets/ICDAR2017-CLAMM/StyleClassification/ \
+ --model-name densenet121 --epochs 50 --experiment-name classification --output-folder ../output/ --ignoregit --lr 0.01 --momentum 0.9 \
+ --decay-lr 20
 
-python ./template/RunMe.py --runner-class image_classification --dataset-folder ../datasets/ICDAR2017-CLAMM/StyleClassification/
---model-name 121 --epochs 50 --experiment-name classification --output-folder ../output/ --ignoregit --lr 0.01 --momentum 0.9
---decay-lr 20 --pretrained
+python ./template/RunMe.py --runner-class image_classification --dataset-folder ../datasets/ICDAR2017-CLAMM/StyleClassification/ \
+ --model-name densenet121 --epochs 50 --experiment-name classification --output-folder ../output/ --ignoregit --lr 0.01 --momentum 0.9 \
+ --decay-lr 20 --pretrained
+
+#InceptionV3
+python ./template/RunMe.py --runner-class image_classification --dataset-folder ../datasets/ICDAR2017-CLAMM/StyleClassification/ --model-name inception_v3 --epochs 50 --experiment-name classification --output-folder ../output/ --lr 0.01 --momentum 0.9 --decay-lr 20 --ignoregit
+
+python ./template/RunMe.py --runner-class image_classification --dataset-folder ../datasets/ICDAR2017-CLAMM/StyleClassification/ --model-name inception_v3 --epochs 50 --experiment-name classification --output-folder ../output/ --lr 0.01 --momentum 0.9 --decay-lr 20 --pretrained --ignoregit
 
 
--- similarity-experiments-coloredDataset.sh
+++ similarity-experiments-coloredDataset.sh
@@ -3,43 +3,48 @@
 #Experiments for Colored Dataset
 
 # Resnet18 ---------------------------------------
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name resnet18 --epochs 50 --experiment-name similarity --output-folder ../output/similarity/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --output-channels 128 --map auto --ignoregit
+python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name resnet18 --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --ignoregit
 
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name resnet18 --epochs 50 --experiment-name similarity --output-folder ../output/similarity/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --output-channels 128 --map auto --pretrained --ignoregit
-
-# Resnet153 ---------------------------------------
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name resnet153 --epochs 50 --experiment-name similarity --output-folder ../output/similarity/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --output-channels 128 --map auto --ignoregit
-
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name resnet153 --epochs 50 --experiment-name similarity --output-folder ../output/similarity/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --output-channels 128 --map auto --pretrained --ignoregit
-
-# VGG19 ---------------------------------------
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name vgg19 --epochs 50 --experiment-name similarity --output-folder ../output/similarity/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --output-channels 128 --map auto --ignoregit
-
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name vgg19 --epochs 50 --experiment-name similarity --output-folder ../output/similarity/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --output-channels 128 --map auto --pretrained --ignoregit
-
-# VGG19_bn ---------------------------------------
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name vgg19_bn --epochs 50 --experiment-name similarity --output-folder ../output/similarity/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --output-channels 128 --map auto --ignoregit
-
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name vgg19_bn --epochs 50 --experiment-name similarity --output-folder ../output/similarity/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --output-channels 128 --map auto --pretrained --ignoregit
-
-# VGG11 ---------------------------------------
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name vgg11 --epochs 50 --experiment-name similarity --output-folder ../output/similarity/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --output-channels 128 --map auto --ignoregit
-
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name vgg11 --epochs 50 --experiment-name similarity --output-folder ../output/similarity/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --output-channels 128 --map auto --pretrained --ignoregit
-
-# VGG11_bn ---------------------------------------
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name vgg11_bn --epochs 50 --experiment-name similarity --output-folder ../output/similarity/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --output-channels 128 --map auto --ignoregit
-
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name vgg11_bn --epochs 50 --experiment-name similarity --output-folder ../output/similarity/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --output-channels 128 --map auto --pretrained --ignoregit
+python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name resnet18 --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --pretrained --ignoregit
 
 # Densenet121 ---------------------------------------
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name densenet121 --epochs 50 --experiment-name similarity --output-folder ../output/similarity/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --output-channels 128 --map auto --ignoregit
+python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name densenet121 --epochs 5 --experiment-name similarity --output-folder ../output/similarity/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --ignoregit
 
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name densenet121 --epochs 50 --experiment-name similarity --output-folder ../output/similarity/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --output-channels 128 --map auto --pretrained --ignoregit
+python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name densenet121 --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --pretrained --ignoregit
+
+#InceptionV3
+python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name inception_v3 --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --ignoregit
+
+python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name inception_v3 --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --pretrained --ignoregit
+
+# VGG19 ---------------------------------------
+python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name vgg19 --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --ignoregit
+
+python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name vgg19 --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --pretrained --ignoregit
+
+# VGG19_bn ---------------------------------------
+python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name vgg19_bn --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --ignoregit
+
+python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name vgg19_bn --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --pretrained --ignoregit
+
+# VGG11 ---------------------------------------
+python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name vgg11 --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --ignoregit
+
+python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name vgg11 --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --pretrained --ignoregit
+
+# VGG11_bn ---------------------------------------
+python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name vgg11_bn --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --ignoregit
+
+python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name vgg11_bn --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --pretrained --ignoregit
 
 # Densenet201 ---------------------------------------
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name densenet201 --epochs 50 --experiment-name similarity --output-folder ../output/similarity/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --output-channels 128 --map auto --ignoregit
+python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name densenet201 --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --ignoregit
 
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name densenet201 --epochs 50 --experiment-name similarity --output-folder ../output/similarity/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --output-channels 128 --map auto --pretrained --ignoregit
+python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name densenet201 --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --pretrained --ignoregit
+
+# Resnet153 ---------------------------------------
+python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name resnet153 --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --ignoregit
+
+python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name resnet153 --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --pretrained --ignoregit
 
 
