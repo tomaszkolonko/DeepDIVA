e05ad07b9b9d80537fffc53c963eb89c3200017c
Thomas Kolonko <tz.kolonko@gmail.com>
2018-12-07 13:00:23 +0100

    another try

--- models/HQ_ResNet.py
+++ models/HQ_ResNet.py
@@ -102,24 +102,21 @@ class _HQ_ResNet(nn.Module):
         self.inplanes = 128
         super(_HQ_ResNet, self).__init__()
 
-        self.expected_input_size = (454, 454)
+        self.expected_input_size = (460, 460)
 
-        self.conv00 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=0,
+        # TODO: A new convolution and batch norm is defined
+        self.conv00 = nn.Conv2d(3, 16, kernel_size=7, stride=1, padding=0,
                                 bias=False)
         self.bn00 = nn.BatchNorm2d(16)
 
-        self.conv01 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=0,
+        # TODO: A new convolution and batch norm is defined
+        self.conv01 = nn.Conv2d(16, 32, kernel_size=7, stride=1, padding=0,
                                 bias=False)
         self.bn01 = nn.BatchNorm2d(32)
 
-        self.conv02 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=0,
+        self.conv02 = nn.Conv2d(32, 64, kernel_size=7, stride=2, padding=3,
                                 bias=False)
         self.bn02 = nn.BatchNorm2d(64)
-
-        self.conv03 = nn.Conv2d(64, 64, kernel_size=7, stride=2, padding=3,
-                                bias=False)
-        self.bn03 = nn.BatchNorm2d(64)
-        # ReLU reused and not self-implemented
 
         # TODO: only the filter dimensions are changed now
         self.conv1 = nn.Conv2d(64, 128, kernel_size=7, stride=2, padding=3,
@@ -175,10 +172,6 @@ class _HQ_ResNet(nn.Module):
 
         x = self.conv02(x)
         x = self.bn02(x)
-        x = self.relu(x)
-
-        x = self.conv03(x)
-        x = self.bn03(x)
         x = self.relu(x)
 
         x = self.conv1(x)
