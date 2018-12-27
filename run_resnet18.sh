a52792b0ef794002c39f3ee526c062d86090838e
Thomas Kolonko <tz.kolonko@gmail.com>
2018-12-27 11:07:32 +0100

    cleaning up (seperating projects)

--- run_resnet18.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Resnet18 ---------------------------------------
-python ./template/RunMe.py --dataset-folder ../ASBESTOS/ --output-folder ../resnet18/ --model-name resnet18 --epochs 30 --experiment-name classification --ignoregit --lr 0.01 --optimizer-name Adam --decay-lr 5 --seed 42
-
-python ./template/RunMe.py --dataset-folder ../ASBESTOS/ --output-folder ../resnet18/ --model-name resnet18 --epochs 30 --experiment-name classification --ignoregit --lr 0.01 --optimizer-name Adam --decay-lr 5 --seed 42 --pretrained
