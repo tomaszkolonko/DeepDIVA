3c58c3e1ed3c070276aeaf88ae6603e5b156fc43
LindaSt <lin.studer@gmail.com>
2019-01-11 13:48:32 +0100

    added new bash for similarity longrun

--- similarity-experiments-coloredDataset.sh
+++ similarity-experiments-coloredDataset.sh
@@ -1,50 +1,34 @@
 #!/bin/bash
 
-#Experiments for Colored Dataset
-
-# Resnet18 ---------------------------------------
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name resnet18 --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --ignoregit
-
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name resnet18 --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --pretrained --ignoregit
-
-# Densenet121 ---------------------------------------
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name densenet121 --epochs 5 --experiment-name similarity --output-folder ../output/similarity/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --ignoregit
-
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name densenet121 --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --pretrained --ignoregit
+#*********************************************************************************************
+#Experiments for Colored Dataset PRETRAINED
 
 #InceptionV3
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name inception_v3 --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --ignoregit
-
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name inception_v3 --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --pretrained --ignoregit
-
-# VGG19 ---------------------------------------
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name vgg19 --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --ignoregit
-
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name vgg19 --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --pretrained --ignoregit
+python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi_col_small/ --model-name inception_v3 --epochs 5 --experiment-name ls-sigopt-similarity-randomcrop-pretrained-inception_v3 --output-folder ../output/ --decay-lr 20 --n-triplets 50000 --output-channels 128 --map auto --pretrained --ignoregit --regenerate-every 1 -j 16 --sig-opt-token LVVXZLFGZSCTCTAXCIXXTWFZUFLYSLRCIUGEZXAMXRXAXYGD --sig-opt-runs 3 --sig-opt util/sigopt-simpretrained.json --batch-size 32
 
 # VGG19_bn ---------------------------------------
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name vgg19_bn --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --ignoregit
-
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name vgg19_bn --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --pretrained --ignoregit
-
-# VGG11 ---------------------------------------
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name vgg11 --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --ignoregit
-
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name vgg11 --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --pretrained --ignoregit
-
-# VGG11_bn ---------------------------------------
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name vgg11_bn --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --ignoregit
-
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name vgg11_bn --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --pretrained --ignoregit
-
-# Densenet201 ---------------------------------------
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name densenet201 --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --ignoregit
-
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name densenet201 --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --pretrained --ignoregit
+python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi_col_small/ --model-name vgg19_bn --epochs 5 --experiment-name ls-sigopt-similarity-randomcrop-pretrained-vgg19_bn --output-folder ../output/ --decay-lr 20 --n-triplets 50000 --output-channels 128 --map auto --pretrained --ignoregit --regenerate-every 1 -j 16 --sig-opt-token LVVXZLFGZSCTCTAXCIXXTWFZUFLYSLRCIUGEZXAMXRXAXYGD --sig-opt-runs 3 --sig-opt util/sigopt-simpretrained.json --batch-size 32
 
 # Resnet152 ---------------------------------------
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name resnet152 --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --ignoregit
+python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi_col_small/ --model-name resnet152 --epochs 5 --experiment-name ls-sigopt-similarity-randomcrop-pretrained-resnet152 --output-folder ../output/ --decay-lr 20 --n-triplets 50000 --output-channels 128 --map auto --pretrained --ignoregit --regenerate-every 1 -j 16 --sig-opt-token LVVXZLFGZSCTCTAXCIXXTWFZUFLYSLRCIUGEZXAMXRXAXYGD --sig-opt-runs 3 --sig-opt util/sigopt-simpretrained.json --batch-size 32
 
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi/ColoredDataset/ --model-name resnet152 --epochs 5 --experiment-name similarity --output-folder ../output/Colored/ --lr 0.01 --momentum 0.9 --decay-lr 20 --n-triplets 100000 --output-channels 128 --map auto --pretrained --ignoregit
+# Densenet121 ---------------------------------------
+python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi_col_small/ --model-name densenet121 --epochs 5 --experiment-name ls-sigopt-similarity-randomcrop-pretrained-densenet121 --output-folder ../output/ --decay-lr 20 --n-triplets 50000 --output-channels 128 --map auto --pretrained --ignoregit --regenerate-every 1 -j 16 --sig-opt-token LVVXZLFGZSCTCTAXCIXXTWFZUFLYSLRCIUGEZXAMXRXAXYGD --sig-opt-runs 3 --sig-opt util/sigopt-simpretrained.json --batch-size 32
 
 
+exit 1
+
+
+#Experiments for Colored Dataset NOT PRETRAINED
+
+# VGG19_bn ---------------------------------------
+python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi_col_small/ --model-name vgg19_bn --epochs 5 --experiment-name ls-sigopt-similarity-randomcrop-vgg19_bn --output-folder ../output/ --decay-lr 25 --n-triplets 50000 --output-channels 128 --map auto --ignoregit --sig-opt-token LVVXZLFGZSCTCTAXCIXXTWFZUFLYSLRCIUGEZXAMXRXAXYGD --sig-opt-runs 10 --sig-opt util/sigopt.json --batch-size 32 --regenerate-every 1 -j 16
+
+#InceptionV3
+python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi_col_small/ --model-name inception_v3 --epochs 5 --experiment-name ls-sigopt-similarity-randomcrop-inception_v3 --output-folder ../output/ --decay-lr 25 --n-triplets 50000 --output-channels 128 --map auto --ignoregit --sig-opt-token LVVXZLFGZSCTCTAXCIXXTWFZUFLYSLRCIUGEZXAMXRXAXYGD --sig-opt-runs 10 --sig-opt util/sigopt.json --batch-size 32 --regenerate-every 1 -j 16
+
+# Resnet152 ---------------------------------------
+python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi_col_small/ --model-name resnet152 --epochs 5 --experiment-name ls-sigopt-similarity-randomcrop-resnet152 --output-folder ../output/ --decay-lr 25 --n-triplets 50000 --output-channels 128 --map auto --ignoregit --sig-opt-token LVVXZLFGZSCTCTAXCIXXTWFZUFLYSLRCIUGEZXAMXRXAXYGD --sig-opt-runs 5 --sig-opt util/sigopt.json --batch-size 32 --regenerate-every 1 -j 16
+
+# Densenet121 ---------------------------------------
+python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi_col_small/ --model-name densenet121 --epochs 5 --experiment-name ls-sigopt-similarity-randomcrop-densenet121 --output-folder ../output/ --decay-lr 25 --n-triplets 50000 --output-channels 128 --map auto --ignoregit --sig-opt-token LVVXZLFGZSCTCTAXCIXXTWFZUFLYSLRCIUGEZXAMXRXAXYGD --sig-opt-runs 5 --sig-opt util/sigopt.json --batch-size 32 --regenerate-every 1 -j 16
