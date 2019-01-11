9146cc106825976a4867e93b975941bb934eb1ea
LindaSt <lin.studer@gmail.com>
2019-01-11 11:39:59 +0100

    updated similarity experiment bash for pre-trained

--- similarity-experiments-coloredDataset-sigopt.sh
+++ similarity-experiments-coloredDataset-sigopt.sh
@@ -6,9 +6,6 @@
 #*********************************************************************************************
 #Experiments for Colored Dataset PRETRAINED
 
-# Densenet121 ---------------------------------------
-python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi_col_small/ --model-name densenet121 --epochs 5 --experiment-name ls-sigopt-similarity-randomcrop-pretrained-densenet121 --output-folder ../output/ --decay-lr 20 --n-triplets 50000 --output-channels 128 --map auto --pretrained --ignoregit --regenerate-every 1 -j 16 --sig-opt-token LVVXZLFGZSCTCTAXCIXXTWFZUFLYSLRCIUGEZXAMXRXAXYGD --sig-opt-runs 3 --sig-opt util/sigopt-simpretrained.json --batch-size 32
-
 #InceptionV3
 python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi_col_small/ --model-name inception_v3 --epochs 5 --experiment-name ls-sigopt-similarity-randomcrop-pretrained-inception_v3 --output-folder ../output/ --decay-lr 20 --n-triplets 50000 --output-channels 128 --map auto --pretrained --ignoregit --regenerate-every 1 -j 16 --sig-opt-token LVVXZLFGZSCTCTAXCIXXTWFZUFLYSLRCIUGEZXAMXRXAXYGD --sig-opt-runs 3 --sig-opt util/sigopt-simpretrained.json --batch-size 32
 
@@ -17,6 +14,10 @@ python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/h
 
 # Resnet152 ---------------------------------------
 python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi_col_small/ --model-name resnet152 --epochs 5 --experiment-name ls-sigopt-similarity-randomcrop-pretrained-resnet152 --output-folder ../output/ --decay-lr 20 --n-triplets 50000 --output-channels 128 --map auto --pretrained --ignoregit --regenerate-every 1 -j 16 --sig-opt-token LVVXZLFGZSCTCTAXCIXXTWFZUFLYSLRCIUGEZXAMXRXAXYGD --sig-opt-runs 3 --sig-opt util/sigopt-simpretrained.json --batch-size 32
+
+# Densenet121 ---------------------------------------
+python ./template/RunMe.py --runner-class triplet --dataset-folder ../datasets/historical_wi_col_small/ --model-name densenet121 --epochs 5 --experiment-name ls-sigopt-similarity-randomcrop-pretrained-densenet121 --output-folder ../output/ --decay-lr 20 --n-triplets 50000 --output-channels 128 --map auto --pretrained --ignoregit --regenerate-every 1 -j 16 --sig-opt-token LVVXZLFGZSCTCTAXCIXXTWFZUFLYSLRCIUGEZXAMXRXAXYGD --sig-opt-runs 3 --sig-opt util/sigopt-simpretrained.json --batch-size 32
+
 
 exit 1
 
