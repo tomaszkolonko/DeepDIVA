432e1c257877e74ee062304ae1ebf42433b87386
Thomas Kolonko <tz.kolonko@gmail.com>
2019-01-13 16:23:04 +0100

    updated the scripts for sigopt

--- sigopt_classification_manuscript_pretrained.sh
+++ sigopt_classification_manuscript_pretrained.sh
@@ -4,7 +4,7 @@
 python ./template/RunMe.py --runner-class image_classification \
     --dataset-folder /home/thomas.kolonko/datasets/ICDAR2017-CLAMM/ManuscriptDating \
     --model-name vgg19_bn --epochs 50 --experiment-name tz_class_manuscript_vgg \
-    --output-folder ../output_tz_classification_manuscript/ --ignoregit --lr 0.01 \
+    --output-folder ../output_tz_classification_manuscript/ --ignoregit --lr 0.01 --pretrained \
     --momentum 0.9 --decay-lr 20 --sig-opt-token LVVXZLFGZSCTCTAXCIXXTWFZUFLYSLRCIUGEZXAMXRXAXYGD \
     --sig-opt-runs 30 --sig-opt util/sigopt.json
 
@@ -12,7 +12,7 @@ python ./template/RunMe.py --runner-class image_classification \
 python ./template/RunMe.py --runner-class image_classification \
     --dataset-folder /home/thomas.kolonko/datasets/ICDAR2017-CLAMM/ManuscriptDating \
     --model-name resnet152 --epochs 50 --experiment-name tz_class_manuscript_resnet \
-    --output-folder ../output_tz_classification_manuscript/ --ignoregit --lr 0.01 \
+    --output-folder ../output_tz_classification_manuscript/ --ignoregit --lr 0.01 --pretrained \
     --momentum 0.9 --decay-lr 20 --sig-opt-token LVVXZLFGZSCTCTAXCIXXTWFZUFLYSLRCIUGEZXAMXRXAXYGD \
     --sig-opt-runs 30 --sig-opt util/sigopt.json
 
@@ -20,7 +20,7 @@ python ./template/RunMe.py --runner-class image_classification \
 python ./template/RunMe.py --runner-class image_classification \
     --dataset-folder /home/thomas.kolonko/datasets/ICDAR2017-CLAMM/ManuscriptDating \
     --model-name inception_v3 --epochs 50 --experiment-name tz_class_manuscript_inception \
-    --output-folder ../output_tz_classification_manuscript/ --ignoregit --lr 0.01 \
+    --output-folder ../output_tz_classification_manuscript/ --ignoregit --lr 0.01 --pretrained \
     --momentum 0.9 --decay-lr 20 --sig-opt-token LVVXZLFGZSCTCTAXCIXXTWFZUFLYSLRCIUGEZXAMXRXAXYGD \
     --sig-opt-runs 30 --sig-opt util/sigopt.json
 
@@ -28,6 +28,6 @@ python ./template/RunMe.py --runner-class image_classification \
 python ./template/RunMe.py --runner-class image_classification \
     --dataset-folder /home/thomas.kolonko/datasets/ICDAR2017-CLAMM/ManuscriptDating \
     --model-name densenet121 --epochs 50 --experiment-name tz_class_manuscript_densenet \
-    --output-folder ../output_tz_classification_manuscript/ --ignoregit --lr 0.01 \
+    --output-folder ../output_tz_classification_manuscript/ --ignoregit --lr 0.01 --pretrained \
     --momentum 0.9 --decay-lr 20 --sig-opt-token LVVXZLFGZSCTCTAXCIXXTWFZUFLYSLRCIUGEZXAMXRXAXYGD \
     --sig-opt-runs 30 --sig-opt util/sigopt.json
--- sigopt_classification_style_pretrained.sh
+++ sigopt_classification_style_pretrained.sh
@@ -3,31 +3,31 @@
 # VGG19_bn ----------------------------------------
 python ./template/RunMe.py --runner-class image_classification \
     --dataset-folder /home/thomas.kolonko/datasets/ICDAR2017-CLAMM/StyleClassification \
-    --model-name vgg19_bn --epochs 50 --experiment-name tz_class_style_vgg \
-    --output-folder ../output_tz_classification_style/ --ignoregit --lr 0.01 \
+    --model-name vgg19_bn --epochs 50 --experiment-name tz_class_style_vgg_pre \
+    --output-folder ../output_tz_classification_style/ --ignoregit --lr 0.01 --pretrained \
     --momentum 0.9 --decay-lr 20 --sig-opt-token LVVXZLFGZSCTCTAXCIXXTWFZUFLYSLRCIUGEZXAMXRXAXYGD \
     --sig-opt-runs 30 --sig-opt util/sigopt.json
 
 # Resnet152 ---------------------------------------
 python ./template/RunMe.py --runner-class image_classification \
     --dataset-folder /home/thomas.kolonko/datasets/ICDAR2017-CLAMM/StyleClassification \
-    --model-name resnet152 --epochs 50 --experiment-name tz_class_style_resnet \
-    --output-folder ../output_tz_classification_style/ --ignoregit --lr 0.01 \
+    --model-name resnet152 --epochs 50 --experiment-name tz_class_style_resnet_pre \
+    --output-folder ../output_tz_classification_style/ --ignoregit --lr 0.01 --pretrained \
     --momentum 0.9 --decay-lr 20 --sig-opt-token LVVXZLFGZSCTCTAXCIXXTWFZUFLYSLRCIUGEZXAMXRXAXYGD \
     --sig-opt-runs 30 --sig-opt util/sigopt.json
 
 # Incecption_v3 -----------------------------------
 python ./template/RunMe.py --runner-class image_classification \
     --dataset-folder /home/thomas.kolonko/datasets/ICDAR2017-CLAMM/StyleClassification \
-    --model-name inception_v3 --epochs 50 --experiment-name tz_class_style_inception \
-    --output-folder ../output_tz_classification_style/ --ignoregit --lr 0.01 \
+    --model-name inception_v3 --epochs 50 --experiment-name tz_class_style_inception_pre \
+    --output-folder ../output_tz_classification_style/ --ignoregit --lr 0.01 --pretraine \
     --momentum 0.9 --decay-lr 20 --sig-opt-token LVVXZLFGZSCTCTAXCIXXTWFZUFLYSLRCIUGEZXAMXRXAXYGD \
     --sig-opt-runs 30 --sig-opt util/sigopt.json
 
 # Densenet121 -------------------------------------
 python ./template/RunMe.py --runner-class image_classification \
     --dataset-folder /home/thomas.kolonko/datasets/ICDAR2017-CLAMM/StyleClassification \
-    --model-name densenet121 --epochs 50 --experiment-name tz_class_style_densenet \
-    --output-folder ../output_tz_classification_style/ --ignoregit --lr 0.01 \
+    --model-name densenet121 --epochs 50 --experiment-name tz_class_style_densnet_pre \
+    --output-folder ../output_tz_classification_style/ --ignoregit --lr 0.01 --pretraine \
     --momentum 0.9 --decay-lr 20 --sig-opt-token LVVXZLFGZSCTCTAXCIXXTWFZUFLYSLRCIUGEZXAMXRXAXYGD \
     --sig-opt-runs 30 --sig-opt util/sigopt.json
