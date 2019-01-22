removed weighted cm calculation, fixed cm heatmap if cm did not work

template/runner/semantic_segmentation_hisdb/evaluate.py
@@ -337,23 +337,25 @@ def validate(data_loader, model, criterion, writer, epoch, class_names, dataset_
         # load the weights
         weights = _load_class_frequencies_weights_from_file(dataset_folder, inmem, workers, runner_class)
         # calculate confusion matrices
-        # sample_weight = [weights[i] for i in np.array(targets).flatten()]
         cm = confusion_matrix(y_true=np.array(targets).flatten(), y_pred=np.array(preds).flatten(), labels=[i for i in range(num_classes)])
-        #cm_w = confusion_matrix(y_true=np.array(targets).flatten(), y_pred=np.array(preds).flatten(), labels=[i for i in range(num_classes)],
-        #                        sample_weight=[weights[i] for i in np.array(targets).flatten()])
         confusion_matrix_heatmap = make_heatmap(cm, class_names)
-        #confusion_matrix_heatmap_w = confusion_matrix_heatmap
+
+        # sample_weight = [weights[i] for i in np.array(targets).flatten()]
+        # cm_w = confusion_matrix(y_true=np.array(targets).flatten(), y_pred=np.array(preds).flatten(), labels=[i for i in range(num_classes)],
+        #                        sample_weight=[weights[i] for i in np.array(targets).flatten()])
         #confusion_matrix_heatmap_w = make_heatmap(np.round(cm_w).astype(np.int), class_names)
     except ValueError:
         logging.warning('Confusion Matrix did not work as expected')
-        confusion_matrix_heatmap = np.zeros((10, 10, 3))
-        confusion_matrix_heatmap_w = confusion_matrix_heatmap
+        confusion_matrix_heatmap = np.zeros((10, 10, 3), dtype=np.int)
+        #confusion_matrix_heatmap_w = confusion_matrix_heatmap
 
     # Logging the epoch-wise accuracy and saving the confusion matrix
     if multi_run is None:
+        print(np.max(confusion_matrix_heatmap))
         writer.add_scalar(logging_label + '/meanIU', meanIU.avg, epoch)
         save_image_and_log_to_tensorboard(writer, tag=logging_label + '/confusion_matrix',
                                           image=confusion_matrix_heatmap, global_step=epoch)
+
         # save_image_and_log_to_tensorboard(writer, tag=logging_label + '/confusion_matrix_weighted',
         #                                   image=confusion_matrix_heatmap_w, global_step=epoch)
     else:

util/misc.py
@@ -246,8 +246,9 @@ def tensor_to_image(image):
     assert (image.shape[2] == 3)  # Last channel is of size 3 for RGB
 
     # Check that the range is [0:255]
-    image = (image - image.min()) / (image.max() - image.min())
-    image = image * 255
+    if np.any(image): # if all zero array -> divison by zero leads to nan
+        image = (image - image.min()) / (image.max() - image.min())
+        image = image * 255
     assert (image.min() >= 0)  # Data should be in range [0:255]
 
     return image