124d8f67a9c26ad7a52e553528cf1798893f2b23
LindaSt <lin.studer@gmail.com>
2019-02-05 22:41:52 +0100

    added bash for segmentation experiments

--- segmentation-experiments-CB55-sigopt.sh
+++ segmentation-experiments-CB55-sigopt.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
+#Experiments for HisDB-CS18
+
+# Unet ---------------------------------------
+python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder /home/ltp/dataset/HisDB-private/CS18/ --model-name Unet --epochs 30 --experiment-name ls-sigopt-segm-Unet-CS18 --output-folder ../output/ --decay-lr 15 --ignoregit --sig-opt-token LVVXZLFGZSCTCTAXCIXXTWFZUFLYSLRCIUGEZXAMXRXAXYGD --sig-opt-runs 10 --sig-opt util/sigopt.json --batch-size 32 --crop-size 256 --pages-in-memory 3 --crops-per-page 200
+
+
+# fcdensenet67 ---------------------------------------
+python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder /home/ltp/dataset/HisDB-private/CS18/ --model-name fcdensenet57 --epochs 30 --experiment-name ls-sigopt-segm-fcdensenet57-CS18 --output-folder ../output/ --decay-lr 15 --ignoregit --sig-opt-token LVVXZLFGZSCTCTAXCIXXTWFZUFLYSLRCIUGEZXAMXRXAXYGD --sig-opt-runs 10 --sig-opt util/sigopt.json --batch-size 32 --crop-size 256 --pages-in-memory 3 --crops-per-page 200
+
+
+#Experiments for HisDB-CS863
+
+# Unet ---------------------------------------
+python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder /home/ltp/dataset/HisDB-private/CS18/ --model-name Unet --epochs 30 --experiment-name ls-sigopt-segm-Unet-CS18 --output-folder ../output/ --decay-lr 15 --ignoregit --sig-opt-token LVVXZLFGZSCTCTAXCIXXTWFZUFLYSLRCIUGEZXAMXRXAXYGD --sig-opt-runs 10 --sig-opt util/sigopt.json --batch-size 32 --crop-size 256 --pages-in-memory 3 --crops-per-page 200
+
+
+# fcdensenet67 ---------------------------------------
+python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder /home/ltp/dataset/HisDB-private/CS18/ --model-name fcdensenet57 --epochs 30 --experiment-name ls-sigopt-segm-fcdensenet57-CS18 --output-folder ../output/ --decay-lr 15 --ignoregit --sig-opt-token LVVXZLFGZSCTCTAXCIXXTWFZUFLYSLRCIUGEZXAMXRXAXYGD --sig-opt-runs 10 --sig-opt util/sigopt.json --batch-size 32 --crop-size 256 --pages-in-memory 3 --crops-per-page 200
+
+#Experiments for HisDB-55
+
+# Unet ---------------------------------------
+python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder /home/ltp/dataset/HisDB/CB55/ --model-name Unet --epochs 30 --experiment-name ls-sigopt-segm-Unet --output-folder ../output/ --decay-lr 15 --ignoregit --sig-opt-token LVVXZLFGZSCTCTAXCIXXTWFZUFLYSLRCIUGEZXAMXRXAXYGD --sig-opt-runs 10 --sig-opt util/sigopt.json --batch-size 6 --crop-size 256 --pages-in-memory 3 --crops-per-page 200
+
+
+
+exit 1
+
 #Experiments for HisDB-55
 
 # Unet ---------------------------------------
--- segmentation-experiments-optimized.sh
+++ segmentation-experiments-optimized.sh
@@ -3,11 +3,11 @@
 #Experiments for HisDB-55
 
 # Unet ---------------------------------------
-python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder ../datasets/HisDB/CB55/ --model-name Unet --epochs 60 --experiment-name segmentation --output-folder ../output/ --decay-lr 28 --ignoregit \
+python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder ../datasets/HisDB-private/CB55/ --model-name Unet --epochs 60 --experiment-name segmentation --output-folder ../output/ --decay-lr 28 --ignoregit \
  --batch-size 32 -j 8 --crop-size 256 --pages-in-memory 3 --crops-per-page 1000 --momentum 0.11280 --weight-decay 0.00594 --lr 0.16824 \
  --disable-databalancing --use-boundary-pixel --no-val-conf-matrix
 
 # fcdensenet67 ---------------------------------------
-python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder ../datasets/HisDB/CB55/ --model-name fcdensenet57 --epochs 60 --experiment-name segmentation --output-folder ../output/ --decay-lr 28 \
+python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder ../datasets/HisDB-private/CB55/ --model-name fcdensenet57 --epochs 60 --experiment-name segmentation --output-folder ../output/ --decay-lr 28 \
  --batch-size 32 -j 8 --crop-size 256 --pages-in-memory 3 --crops-per-page 1000 --momentum 0.32149 --weight-decay 0.00549 --lr 0.09272 \
  --disable-databalancing --use-boundary-pixel --no-val-conf-matrix
