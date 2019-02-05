f5d9aeb061d3290254b79063f4e10bd3f2316e3e
LindaSt <lin.studer@gmail.com>
2019-02-05 23:09:50 +0100

    added bash for segmentation experiments

--- segmentation-experiments-CB55-sigopt.sh
+++ segmentation-experiments-CB55-sigopt.sh
@@ -3,7 +3,7 @@
 #Experiments for HisDB-CS18
 
 # Unet ---------------------------------------
-    python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder ../datasets/HisDB-private/CS18/ --model-name Unet --epochs 30 --experiment-name ls-sigopt-segm-Unet-CS18 --output-folder ../output/ --decay-lr 15 --ignoregit --sig-opt-token LVVXZLFGZSCTCTAXCIXXTWFZUFLYSLRCIUGEZXAMXRXAXYGD --sig-opt-runs 10 --sig-opt util/sigopt.json --batch-size 32 --crop-size 256 --pages-in-memory 3 --crops-per-page 200 -j 8
+python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder ../datasets/HisDB-private/CS18/ --model-name Unet --epochs 30 --experiment-name ls-sigopt-segm-Unet-CS18 --output-folder ../output/ --decay-lr 15 --ignoregit --sig-opt-token LVVXZLFGZSCTCTAXCIXXTWFZUFLYSLRCIUGEZXAMXRXAXYGD --sig-opt-runs 10 --sig-opt util/sigopt.json --batch-size 32 --crop-size 256 --pages-in-memory 3 --crops-per-page 200 -j 8
 
 # fcdensenet67 ---------------------------------------
 python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder ../datasets/HisDB-private/CS18/ --model-name fcdensenet57 --epochs 30 --experiment-name ls-sigopt-segm-fcdensenet57-CS18 --output-folder ../output/ --decay-lr 15 --ignoregit --sig-opt-token LVVXZLFGZSCTCTAXCIXXTWFZUFLYSLRCIUGEZXAMXRXAXYGD --sig-opt-runs 10 --sig-opt util/sigopt.json --batch-size 32 --crop-size 256 --pages-in-memory 3 --crops-per-page 200 -j 8
