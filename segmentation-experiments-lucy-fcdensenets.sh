c4ae205f0266ae4befec648095ae638c2cb84cde
LindaSt <lin.studer@gmail.com>
2019-02-08 18:03:49 +0100

    added bash for segmentation experiments

--- segmentation-experiments-lucy-fcdensenets.sh
+++ segmentation-experiments-lucy-fcdensenets.sh
@@ -3,7 +3,7 @@
 #Experiments for HisDB-55
 
 # fcdensenet67 ---------------------------------------
-python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder ../datasets/HisDB-private/CB55/ --model-name fcdensenet57 --epochs 50 --experiment-name segmentation --output-folder ../output/ --decay-lr 24 \
+python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder /data/ltp/HisDB-private/CB55/ --model-name fcdensenet57 --epochs 50 --experiment-name segmentation --output-folder ../output/ --decay-lr 24 \
  --batch-size 32 -j 16 --crop-size 256 --pages-in-memory 3 --crops-per-page 1000 --momentum 0.9 --lr 0.005 \
  --disable-databalancing --use-boundary-pixel --no-val-conf-matrix --gpu-id 0
 
@@ -11,7 +11,7 @@ python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-
 #Experiments for HisDB-CS863
 
 # fcdensenet67 ---------------------------------------
-python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder ../datasets/HisDB-private/CS863/ --model-name fcdensenet57 --epochs 50 --experiment-name segmentation --output-folder ../output/ --decay-lr 24 \
+python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder /data/ltp/HisDB-private/CS863/ --model-name fcdensenet57 --epochs 50 --experiment-name segmentation --output-folder ../output/ --decay-lr 24 \
  --batch-size 32 -j 16 --crop-size 256 --pages-in-memory 3 --crops-per-page 1000 --momentum 0.9 --lr 0.005 \
  --disable-databalancing --use-boundary-pixel --no-val-conf-matrix --gpu-id 0
 
@@ -19,6 +19,6 @@ python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-
 #Experiments for HisDB-CS18
 
 # fcdensenet67 ---------------------------------------
-python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder ../datasets/HisDB-private/CS18/ --model-name fcdensenet57 --epochs 50 --experiment-name segmentation --output-folder ../output/ --decay-lr 24 \
+python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder /data/ltp/HisDB-private/CS18/ --model-name fcdensenet57 --epochs 50 --experiment-name segmentation --output-folder ../output/ --decay-lr 24 \
  --batch-size 32 -j 16 --crop-size 256 --pages-in-memory 3 --crops-per-page 1000 --momentum 0.9 --lr 0.005 \
  --disable-databalancing --use-boundary-pixel --no-val-conf-matrix --gpu-id 0
