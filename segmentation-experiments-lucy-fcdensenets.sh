8ceaeca9eca839db884e02576fddae0045bbfd3a
LindaSt <lin.studer@gmail.com>
2019-02-08 18:05:36 +0100

    added bash for segmentation experiments

--- segmentation-experiments-lucy-fcdensenets.sh
+++ segmentation-experiments-lucy-fcdensenets.sh
@@ -4,7 +4,7 @@
 
 # fcdensenet67 ---------------------------------------
 python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder /data/ltp/HisDB-private/CB55/ --model-name fcdensenet57 --epochs 50 --experiment-name segmentation --output-folder ../output/ --decay-lr 24 \
- --batch-size 32 -j 16 --crop-size 256 --pages-in-memory 3 --crops-per-page 1000 --momentum 0.9 --lr 0.005 \
+ --ignoregit --batch-size 32 -j 16 --crop-size 256 --pages-in-memory 3 --crops-per-page 1000 --momentum 0.9 --lr 0.005 \
  --disable-databalancing --use-boundary-pixel --no-val-conf-matrix --gpu-id 0
 
 
@@ -12,7 +12,7 @@ python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-
 
 # fcdensenet67 ---------------------------------------
 python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder /data/ltp/HisDB-private/CS863/ --model-name fcdensenet57 --epochs 50 --experiment-name segmentation --output-folder ../output/ --decay-lr 24 \
- --batch-size 32 -j 16 --crop-size 256 --pages-in-memory 3 --crops-per-page 1000 --momentum 0.9 --lr 0.005 \
+ --ignoregit --batch-size 32 -j 16 --crop-size 256 --pages-in-memory 3 --crops-per-page 1000 --momentum 0.9 --lr 0.005 \
  --disable-databalancing --use-boundary-pixel --no-val-conf-matrix --gpu-id 0
 
 
@@ -20,5 +20,5 @@ python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-
 
 # fcdensenet67 ---------------------------------------
 python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder /data/ltp/HisDB-private/CS18/ --model-name fcdensenet57 --epochs 50 --experiment-name segmentation --output-folder ../output/ --decay-lr 24 \
- --batch-size 32 -j 16 --crop-size 256 --pages-in-memory 3 --crops-per-page 1000 --momentum 0.9 --lr 0.005 \
+ --ignoregit --batch-size 32 -j 16 --crop-size 256 --pages-in-memory 3 --crops-per-page 1000 --momentum 0.9 --lr 0.005 \
  --disable-databalancing --use-boundary-pixel --no-val-conf-matrix --gpu-id 0
