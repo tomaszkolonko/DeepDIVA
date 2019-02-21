f62a6dd6d0786def85c2ff23c4efd65f87aee0ba
LindaSt <lin.studer@gmail.com>
2019-02-21 16:16:54 +0100

    updated bash script for pretrained

--- segmentation-experiments-lucy.sh
+++ segmentation-experiments-lucy.sh
@@ -5,14 +5,14 @@
 # deeplabv3 ---------------------------------------
 python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder /data/ltp/HisDB-private/CB55/ --model-name deeplabv3 --epochs 50 --experiment-name segmentation --output-folder ../output/ --decay-lr 24 \
  --ignoregit --batch-size 32 --crop-size 256 --imgs-in-memory 3 --crops-per-image 1000 --momentum 0.9 --lr 0.005 \
- --disable-databalancing --use-boundary-pixel --no-val-conf-matrix --gpu-id 3 -j 32
+ --disable-databalancing --use-boundary-pixel --no-val-conf-matrix --gpu-id 3 -j 32 --pretrained --myclone-env
 
 #Experiments for HisDB-CS863
 
 # deeplabv3 ---------------------------------------
 python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder /data/ltp/HisDB-private/CS863/ --model-name deeplabv3 --epochs 50 --experiment-name segmentation --output-folder ../output/ --decay-lr 24 \
  --ignoregit --batch-size 32 --crop-size 256 --imgs-in-memory 3 --crops-per-image 1000 --momentum 0.9 --lr 0.005 \
- --disable-databalancing --use-boundary-pixel --no-val-conf-matrix --gpu-id 3 -j 32
+ --disable-databalancing --use-boundary-pixel --no-val-conf-matrix --gpu-id 3 -j 32 --pretrained --myclone-env
 
 
 #Experiments for HisDB-CS18
