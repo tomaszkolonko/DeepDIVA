b0b97f756060ca0f9b48e2d96b5072d9a44ebcd8
LindaSt <lin.studer@gmail.com>
2019-02-23 16:50:37 +0100

    changed transform back to not multi-crop

--- classification-kmnist-experiments-dave.sh
+++ classification-kmnist-experiments-dave.sh
@@ -4,38 +4,38 @@
 
 # Resnet152 -------------------------------------------------------
 python ./template/RunMe.py --runner-class image_classification \
-    --dataset-folder ../datasets/KMNIST --model-name resnet152 --epochs 100 --experiment-name classification \
+    --dataset-folder ../datasets/KMNIST --model-name resnet152 --epochs 35 --experiment-name classification \
     --output-folder ../output --ignoregit --lr 0.01 --momentum 0.9 --decay-lr 30
 
 python ./template/RunMe.py --runner-class image_classification \
-    --dataset-folder ../datasets/KMNIST --model-name resnet152 --epochs 100 --experiment-name classification \
+    --dataset-folder ../datasets/KMNIST --model-name resnet152 --epochs 35 --experiment-name classification \
     --output-folder ../output --ignoregit --lr 0.01 --momentum 0.9 --decay-lr 30 --pretrained
 
 # VGG19 -------------------------------------------------------
 python ./template/RunMe.py --runner-class image_classification \
-    --dataset-folder ../datasets/KMNIST --model-name vgg19_bn --epochs 100 --experiment-name classification \
+    --dataset-folder ../datasets/KMNIST --model-name vgg19_bn --epochs 35 --experiment-name classification \
     --output-folder ../output --ignoregit --lr 0.01 --momentum 0.9 --decay-lr 30
 
 python ./template/RunMe.py --runner-class image_classification \
-    --dataset-folder ../datasets/KMNIST --model-name vgg19_bn --epochs 100 --experiment-name classification \
+    --dataset-folder ../datasets/KMNIST --model-name vgg19_bn --epochs 35 --experiment-name classification \
     --output-folder ../output --ignoregit --lr 0.01 --momentum 0.9 --decay-lr 30 --pretrained
 
 # densenet121 -------------------------------------------------------
 python ./template/RunMe.py --runner-class image_classification \
-    --dataset-folder ../datasets/KMNIST --model-name densenet121 --epochs 100 --experiment-name classification \
+    --dataset-folder ../datasets/KMNIST --model-name densenet121 --epochs 35 --experiment-name classification \
     --output-folder ../output --ignoregit --lr 0.01 --momentum 0.9 --decay-lr 30
 
 python ./template/RunMe.py --runner-class image_classification \
-    --dataset-folder ../datasets/KMNIST --model-name densenet121 --epochs 100 --experiment-name classification \
+    --dataset-folder ../datasets/KMNIST --model-name densenet121 --epochs 35 --experiment-name classification \
     --output-folder ../output --ignoregit --lr 0.01 --momentum 0.9 --decay-lr 30 --pretrained
 
 # inception -------------------------------------------------------
 python ./template/RunMe.py --runner-class image_classification \
-    --dataset-folder ../datasets/KMNIST --model-name inception_v3 --epochs 100 --experiment-name classification \
+    --dataset-folder ../datasets/KMNIST --model-name inception_v3 --epochs 35 --experiment-name classification \
     --output-folder ../output --ignoregit --lr 0.01 --momentum 0.9 --decay-lr 30
 
 python ./template/RunMe.py --runner-class image_classification \
-    --dataset-folder ../datasets/KMNIST --model-name inception_v3 --epochs 100 --experiment-name classification \
+    --dataset-folder ../datasets/KMNIST --model-name inception_v3 --epochs 35 --experiment-name classification \
     --output-folder ../output --ignoregit --lr 0.01 --momentum 0.9 --decay-lr 30 --pretrained
 
 
@@ -43,36 +43,36 @@ python ./template/RunMe.py --runner-class image_classification \
 
 # Resnet152 -------------------------------------------------------
 python ./template/RunMe.py --runner-class image_classification \
-    --dataset-folder ../datasets/KMNIST --model-name resnet152 --epochs 100 --experiment-name classification \
+    --dataset-folder ../datasets/KMNIST --model-name resnet152 --epochs 35 --experiment-name classification \
     --output-folder ../output --ignoregit --lr 0.001 --momentum 0.9 --decay-lr 30
 
 python ./template/RunMe.py --runner-class image_classification \
-    --dataset-folder ../datasets/KMNIST --model-name resnet152 --epochs 100 --experiment-name classification \
+    --dataset-folder ../datasets/KMNIST --model-name resnet152 --epochs 35 --experiment-name classification \
     --output-folder ../output --ignoregit --lr 0.001 --momentum 0.9 --decay-lr 30 --pretrained
 
 # VGG19 -------------------------------------------------------
 python ./template/RunMe.py --runner-class image_classification \
-    --dataset-folder ../datasets/KMNIST --model-name vgg19_bn --epochs 100 --experiment-name classification \
+    --dataset-folder ../datasets/KMNIST --model-name vgg19_bn --epochs 35 --experiment-name classification \
     --output-folder ../output --ignoregit --lr 0.001 --momentum 0.9 --decay-lr 30
 
 python ./template/RunMe.py --runner-class image_classification \
-    --dataset-folder ../datasets/KMNIST --model-name vgg19_bn --epochs 100 --experiment-name classification \
+    --dataset-folder ../datasets/KMNIST --model-name vgg19_bn --epochs 35 --experiment-name classification \
     --output-folder ../output --ignoregit --lr 0.001 --momentum 0.9 --decay-lr 30 --pretrained
 
 # densenet121 -------------------------------------------------------
 python ./template/RunMe.py --runner-class image_classification \
-    --dataset-folder ../datasets/KMNIST --model-name densenet121 --epochs 100 --experiment-name classification \
+    --dataset-folder ../datasets/KMNIST --model-name densenet121 --epochs 35 --experiment-name classification \
     --output-folder ../output --ignoregit --lr 0.001 --momentum 0.9 --decay-lr 30
 
 python ./template/RunMe.py --runner-class image_classification \
-    --dataset-folder ../datasets/KMNIST --model-name densenet121 --epochs 100 --experiment-name classification \
+    --dataset-folder ../datasets/KMNIST --model-name densenet121 --epochs 35 --experiment-name classification \
     --output-folder ../output --ignoregit --lr 0.001 --momentum 0.9 --decay-lr 30 --pretrained
 
 # inception -------------------------------------------------------
 python ./template/RunMe.py --runner-class image_classification \
-    --dataset-folder ../datasets/KMNIST --model-name inception_v3 --epochs 100 --experiment-name classification \
+    --dataset-folder ../datasets/KMNIST --model-name inception_v3 --epochs 35 --experiment-name classification \
     --output-folder ../output --ignoregit --lr 0.001 --momentum 0.9 --decay-lr 30
 
 python ./template/RunMe.py --runner-class image_classification \
-    --dataset-folder ../datasets/KMNIST --model-name inception_v3 --epochs 100 --experiment-name classification \
+    --dataset-folder ../datasets/KMNIST --model-name inception_v3 --epochs 35 --experiment-name classification \
     --output-folder ../output --ignoregit --lr 0.001 --momentum 0.9 --decay-lr 30 --pretrained
