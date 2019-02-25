137b8ce7645ad9601b8292805df622cfd927d7a0
LindaSt <lin.studer@gmail.com>
2019-02-25 15:39:26 +0100

    fcdensenet can be used with a pretrained model (specified in --path-pretrained-model)

--- segmentation-experiments-pretrained-lucy.sh
+++ segmentation-experiments-pretrained-lucy.sh
@@ -3,7 +3,7 @@
 #Experiments for HisDB-55
 
 # fcdensenet57 pretrained ---------------------------------------
-python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder /data/ltp/HisDB-private/CB55/ --model-name fcdensenet57 pretrained --epochs 50 --experiment-name segmentation --output-folder ../output/ --decay-lr 24 \
+python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder /data/ltp/HisDB-private/CB55/ fcdensenet57 --epochs 50 --experiment-name segmentation --output-folder ../output/ --decay-lr 24 \
  --ignoregit --batch-size 32 --crop-size 256 --imgs-in-memory 3 --crops-per-image 1000 --momentum 0.9 --lr 0.005 \
  --disable-databalancing --use-boundary-pixel --no-val-conf-matrix --gpu-id 0 -j 32 --pretrained \
  --path-pretrained-model /home/linda/output/coco/coco2017/model_name=fcdensenet57/batch_size=10/epochs=50/lr=0.02/momentum=0.9/gpu_id=1/workers=16/no_val_conf_matrix=True/resize_coco=320/20-02-19-13h-32m-49s/model_best.pth.tar
@@ -13,7 +13,7 @@ python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-
 
 # fcdensenet57 pretrained ---------------------------------------
 
-python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder /data/ltp/HisDB-private/CS863/ --model-name fcdensenet57 pretrained --epochs 50 --experiment-name segmentation --output-folder ../output/ --decay-lr 24 \
+python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder /data/ltp/HisDB-private/CS863/ fcdensenet57 --epochs 50 --experiment-name segmentation --output-folder ../output/ --decay-lr 24 \
  --ignoregit --batch-size 32 --crop-size 256 --imgs-in-memory 3 --crops-per-image 1000 --momentum 0.9 --lr 0.005 \
  --disable-databalancing --use-boundary-pixel --no-val-conf-matrix --gpu-id 0 -j 32 --pretrained \
  --path-pretrained-model /home/linda/output/coco/coco2017/model_name=fcdensenet57/batch_size=10/epochs=50/lr=0.02/momentum=0.9/gpu_id=1/workers=16/no_val_conf_matrix=True/resize_coco=320/20-02-19-13h-32m-49s/model_best.pth.tar
@@ -23,7 +23,7 @@ python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-
 
 # fcdensenet57 pretrained ---------------------------------------
 
-python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder /data/ltp/HisDB-private/CS18/ --model-name fcdensenet57 pretrained --epochs 50 --experiment-name segmentation --output-folder ../output/ --decay-lr 24 \
+python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder /data/ltp/HisDB-private/CS18/ fcdensenet57 --epochs 50 --experiment-name test --output-folder ../output/ --decay-lr 24 \
  --ignoregit --batch-size 32 --crop-size 256 --imgs-in-memory 3 --crops-per-image 1000 --momentum 0.9 --lr 0.005 \
  --disable-databalancing --use-boundary-pixel --no-val-conf-matrix --gpu-id 0 -j 32 --pretrained \
  --path-pretrained-model /home/linda/output/coco/coco2017/model_name=fcdensenet57/batch_size=10/epochs=50/lr=0.02/momentum=0.9/gpu_id=1/workers=16/no_val_conf_matrix=True/resize_coco=320/20-02-19-13h-32m-49s/model_best.pth.tar
