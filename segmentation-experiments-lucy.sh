dbb823bf2a4f260561f5dd8b32ce15bb4b9ed591
LindaSt <lin.studer@gmail.com>
2019-03-01 11:20:41 +0100

    updated bash

--- segmentation-experiments-lucy.sh
+++ segmentation-experiments-lucy.sh
@@ -4,11 +4,11 @@
 
 # SegNet ---------------------------------------
 python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder /data/ltp/HisDB-private/CB55/ --model-name SegNet --epochs 50 --experiment-name segmentation --output-folder ../output/ --decay-lr 24 \
- --ignoregit --batch-size 32 --crop-size 256 --imgs-in-memory 3 --crops-per-image 1000 --momentum 0.9 --lr 0.001 \
+ --ignoregit --batch-size 32 --crop-size 256 --imgs-in-memory 3 --crops-per-image 1000 --momentum 0.9 --lr 0.0005 \
  --disable-databalancing --use-boundary-pixel --no-val-conf-matrix --gpu-id 3 -j 32
 
 python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder /data/ltp/HisDB-private/CB55/ --model-name SegNet --epochs 50 --experiment-name segmentation --output-folder ../output/ --decay-lr 24 \
- --ignoregit --batch-size 32 --crop-size 256 --imgs-in-memory 3 --crops-per-image 1000 --momentum 0.9 --lr 0.001 \
+ --ignoregit --batch-size 32 --crop-size 256 --imgs-in-memory 3 --crops-per-image 1000 --momentum 0.9 --lr 0.0005 \
  --disable-databalancing --use-boundary-pixel --no-val-conf-matrix --gpu-id 3 -j 32 --pretrained
 
 
@@ -16,11 +16,11 @@ python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-
 
 # SegNet ---------------------------------------
 python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder /data/ltp/HisDB-private/CS863/ --model-name SegNet --epochs 50 --experiment-name segmentation --output-folder ../output/ --decay-lr 24 \
- --ignoregit --batch-size 32 --crop-size 256 --imgs-in-memory 3 --crops-per-image 1000 --momentum 0.9 --lr 0.001 \
+ --ignoregit --batch-size 32 --crop-size 256 --imgs-in-memory 3 --crops-per-image 1000 --momentum 0.9 --lr 0.0005 \
  --disable-databalancing --use-boundary-pixel --no-val-conf-matrix --gpu-id 3 -j 32 --pretrained
 
 python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder /data/ltp/HisDB-private/CS863/ --model-name SegNet --epochs 50 --experiment-name segmentation --output-folder ../output/ --decay-lr 24 \
- --ignoregit --batch-size 32 --crop-size 256 --imgs-in-memory 3 --crops-per-image 1000 --momentum 0.9 --lr 0.001 \
+ --ignoregit --batch-size 32 --crop-size 256 --imgs-in-memory 3 --crops-per-image 1000 --momentum 0.9 --lr 0.0005 \
  --disable-databalancing --use-boundary-pixel --no-val-conf-matrix --gpu-id 3 -j 32
 
 
@@ -28,11 +28,11 @@ python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-
 
 # SegNet ---------------------------------------
 python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder /data/ltp/HisDB-private/CS18/ --model-name SegNet --epochs 50 --experiment-name segmentation --output-folder ../output/ --decay-lr 24 \
- --ignoregit --batch-size 32 --crop-size 256 --imgs-in-memory 3 --crops-per-image 1000 --momentum 0.9 --lr 0.001 \
+ --ignoregit --batch-size 32 --crop-size 256 --imgs-in-memory 3 --crops-per-image 1000 --momentum 0.9 --lr 0.0005 \
  --disable-databalancing --use-boundary-pixel --no-val-conf-matrix --gpu-id 3 -j 32
 
 python ./template/RunMe.py --runner-class semantic_segmentation_hisdb --dataset-folder /data/ltp/HisDB-private/CS18/ --model-name SegNet --epochs 50 --experiment-name segmentation --output-folder ../output/ --decay-lr 24 \
- --ignoregit --batch-size 32 --crop-size 256 --imgs-in-memory 3 --crops-per-image 1000 --momentum 0.9 --lr 0.001 \
+ --ignoregit --batch-size 32 --crop-size 256 --imgs-in-memory 3 --crops-per-image 1000 --momentum 0.9 --lr 0.0005 \
  --disable-databalancing --use-boundary-pixel --no-val-conf-matrix --gpu-id 3 -j 32 --pretrained
 
 exit 1
